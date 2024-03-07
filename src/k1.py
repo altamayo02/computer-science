05fcdba091226d84bdd323258892f52f8610a773
Alejandro <42224066+altamayo02@users.noreply.github.com>
2024-03-07 00:36:08 -0500

    progress on K = 1

---
+++ src/k1.py
@@ -0,0 +1,76 @@
+import math
+from matplotlib.ticker import MultipleLocator
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# https://stackoverflow.com/a/6800214
+'''from functools import reduce
+def factors(n):
+    return reduce(list.__add__,
+		([i, n // i] for i in range(1, int(n ** 0.5) + 1) if n % i == 0)
+	)'''
+
+# https://www.w3resource.com/python-exercises/python-basic-exercise-31.php
+# Define a function to calculate the greatest common divisor (MCD) of two numbers.
+def mcd(x, y):
+    # Initialize mcd to 1.
+    mcd = 1
+    
+    # Check if y is a divisor of x (x is divisible by y).
+    if x % y == 0:
+        return y
+    
+    # Iterate from half of y down to 1.
+    for k in range(int(y / 2), 0, -1):
+        # Check if both x and y are divisible by k.
+        if x % k == 0 and y % k == 0:
+            # Update the MCD to the current value of k and exit the loop.
+            mcd = k
+            break
+    
+    # Return the calculated MCD.
+    return mcd
+
+def write_csv(content: str, filename: str, notify = False):
+	with open(f"./data/csv/{filename}", "w") as fp:
+		fp.write(content)
+		if notify:
+			print(f"{filename} written successfully!")
+
+def rand_color():
+	return (np.random.random(), np.random.random(), np.random.random(), 1)
+
+X = range(1, 100)
+Y = [n ** 2 for n in X]
+
+figure, axes = plt.subplots()
+plt.grid(True, "both")
+#plt.minorticks_on()
+#axes.yaxis.set_minor_locator(MultipleLocator(100))
+plt.xticks(X)
+plt.ylim(0, 50)
+
+color = rand_color()
+for line in Y:
+	plt.plot(X, [line for _ in X], c=color)
+Y = [n ** 2 + 4 for n in X]
+plt.plot(X, Y, marker="o", c=rand_color())
+#plt.show()
+
+TOP = 1500
+matrix = []
+for n in range(TOP):
+	matrix.append(TOP * [False])
+csv = ""
+for i in range(1, 1 + TOP):
+	for j in range(1, 1 + TOP):
+		if i > j: continue
+		gcd = mcd(i, j)
+		if matrix[(i // gcd) - 1][(j // gcd) - 1]: continue
+		matrix[i - 1][j - 1] = True
+		dist = math.dist([0, 0], [i, j])
+		if dist == math.floor(dist):
+			csv += f"{i},{j},{int(dist)}\n"
+	#print(i)
+write_csv(csv, "terns.csv", True)
