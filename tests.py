d256a4b83208d4a197cd8e474cf7788810612b85
Alejandro <42224066+altamayo02@users.noreply.github.com>
2025-02-27 22:25:57 -0500

    Optimized fundamentals, implemented rank2

---
+++ tests.py
@@ -0,0 +1,6 @@
+def ins():
+	ins = [1, 2, 3, 4, 5, 6]
+	ins.insert(20, None)
+	return ins
+
+print(ins())
--- towers/src/fundamentals.py
+++ towers/src/fundamentals.py
@@ -8,11 +8,26 @@ def permute(lst: list):
 	return perms
 
 def rank(lst: list):
-	# TODO - Implement iteratively, appending either left or right
-	# for less than and greater than
 	opposite = [(v, k) for k, v in enumerate(lst)]
 	ranked = sorted(opposite, key=lambda r: r[0])
 	ranked = [r[1] + 1 for r in ranked]
+	return ranked
+
+def rank2(lst: list):
+	ranked = [1]
+	for i in range(1, len(lst)):
+		j = len(ranked) - 1
+		#print(f'j is {j}')
+		#print(f'Is {lst[i]} < {lst[ranked[j] - 1]}?')
+		while lst[i] < lst[ranked[j] - 1]:
+			#print('Yes')
+			j -= 1
+			#print(f'Is {lst[i]} < {lst[ranked[j] - 1]}?')
+			if j == -1:
+				break
+		#print('No')
+		ranked.insert(j + 1, i + 1)
+		#print(ranked)
 	return ranked
 
 def fundamentals(n: int) -> list:
@@ -24,27 +39,37 @@ def fundamentals(n: int) -> list:
 	for s in sols:
 		if s not in done:
 			upper_s = [n - t + 1 for t in s]
-			s_rev = s[::-1]
-			upper_s_rev = upper_s[::-1]
-			# TODO - Rank less, reverse more
+			#s_rev = s[::-1]
+			s_ranked = rank(s)
+			#upper_s_rev = upper_s[::-1]
 			# 1423 4132
 			# 216354 561423
+			upper_s_ranked = [n + 1 - t for t in s_ranked]
 			for sol in [
 				s,
 				upper_s,
-				rank(s),
-				rank(upper_s),
-				s_rev,
-				upper_s_rev,
-				rank(s_rev),
-				rank(upper_s_rev)
+				#rank(s),
+				s_ranked,
+				#rank(upper_s),
+				upper_s_ranked,
+				#s_rev,
+				s[::-1],
+				#upper_s_rev,
+				upper_s[::-1],
+				#rank(s_rev),
+				s_ranked[::-1],
+				#rank(upper_s_rev)
+				upper_s_ranked[::-1]
 			]:
 				done.append(sol)
 			funds.append(s)
 	
 	return funds
 
-print(len(fundamentals(9)))
+print(len(fundamentals(8)))
+
+#print(rank([2, 4, 3, 1]))
+#print(rank2([2, 4, 3, 1]))
 
 # 1:     1: 1
 # 2:     1: 1
